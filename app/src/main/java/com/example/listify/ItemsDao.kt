4263e62426f9b56dfb77512e6c844ea62d37b15c
Chmpragyan <pragyancham07@gmail.com>
2025-08-09 21:05:57 +0545

    Feat database [wip]: Add items table.

---
+++ app/src/main/java/com/example/listify/ItemsDao.kt
@@ -0,0 +1,20 @@
+package com.example.listify
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ItemsDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item: ListItem)
+
+    @Delete
+    suspend fun delete(item: ListItem)
+
+    @Query("SELECT * FROM items")
+    suspend fun getAllItems(): LiveData<List<ListItem>>
+}
