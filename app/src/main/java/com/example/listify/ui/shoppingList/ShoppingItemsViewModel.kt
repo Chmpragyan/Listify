696a1eb8089a22129729c6614a040ec67f6ed5cd
Chmpragyan <pragyancham07@gmail.com>
2025-08-10 08:54:42 +0545

    Feat viewmodel [WIP]: Setup shoppingItems viewModel.

--- app/src/main/java/com/example/listify/ui/shoppingList/ShoppingItemsViewModel.kt
+++ app/src/main/java/com/example/listify/ui/shoppingList/ShoppingItemsViewModel.kt
@@ -1,5 +1,23 @@
 package com.example.listify.ui.shoppingList
 
-class ShoppingItemsViewModel() {
+import com.example.listify.data.db.model.ShoppingItem
+import com.example.listify.data.repository.ShoppingRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+class ShoppingItemsViewModel(private val shoppingRepository: ShoppingRepository) {
+    fun upsert(items: ShoppingItem) {
+        CoroutineScope(Dispatchers.Main).launch {
+            shoppingRepository.upsert(items)
+        }
+    }
+
+    fun delete(items: ShoppingItem) {
+        CoroutineScope(Dispatchers.Main).launch {
+            shoppingRepository.delete(items)
+        }
+    }
+
+    fun getAllShoppingItems() = shoppingRepository.getAllShoppingItems()
 }
