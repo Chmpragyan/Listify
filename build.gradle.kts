8ce8016eeac0186d61983d1e958f840739972ebf
Pragyan <pragyancham07@gmail.com>
2026-09-18 10:36:54 +0545

    Add SonarQube plugin and configure project properties.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,16 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("androidx.room") version "2.7.2" apply false
+    alias(libs.plugins.sonar)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "chmpragyan_Listify")
+        property("sonar.projectName", "Listify")
+        property("sonar.organization", "chmpragyan")
+        property("sonar.host.url", "http://sonarcloud.io/") // Default, should be overridden by env/CI
+        property("sonar.language", "kotlin")
+        property("sonar.sources", "src/main/java")
+    }
 }
